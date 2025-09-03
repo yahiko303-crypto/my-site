31f59425be189bcbe652e4f9a885bdb51f0d446c
Lance <yahiko303@gmail.com>
2025-09-02 17:24:00 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
+    const id = a.getAttribute('href').slice(1);
+    const target = document.getElementById(id);
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
